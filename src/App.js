8060dcb50533721052bfc14c0b6c10389fddeb9e
Hamad Javed <hamadjaved69@gmail.com>
2021-01-23 05:55:13 +0500

    Added the projects section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Projects from './components/projects/Projects';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Projects />
     </div>
   );
 }
